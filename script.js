d25c30c2351f96e7dcdd479c48c2b75a576f6afc
Selena <selena.k.rodriguez@gmail.com>
2022-10-07 18:23:20 -0500

    Fixed the comparison issue by changing the getUserChoice return from 'choice' to 'userInput' in order to have the number be a string. From there, the game works in comparing the computerChoice string to the userChoice string.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Rock Paper Scissors Instructions:
     // Function getComputerChoice - this will randomly return either "Rock", "Paper", or "Scissors".
-    // Use the getComputerChoice function to have the computer play. (Check with console log before progressing)
+        // Use the getComputerChoice function to have the computer play. (Check with console log before progressing)
     // Need a function that plays a single round of Rock Paper Scissors. (playRound)
-    // Above function needs two parameters. (Make sure to return them to be used in later code.)
-        // playerSelection - needs to not be case sensitive (rock, Rock, RoCk, etc.)
-        // computerSelection
+        // Above function needs two parameters. (Make sure to return them to be used in later code.)
+            // playerSelection - needs to not be case sensitive (rock, Rock, RoCk, etc.) - I called this userChoice
+            // computerSelection - I called this computerChoice
     // Return a string to declare the winner of the round. "You Lose! Paper beats Rock".
     // Function game(), playRound function inside to play a 5 round game. It should keep score and report a winner or loser.
         // use console.log() to display the results of each round and winner at the end.
@@ -16,20 +16,6 @@
         // Paper beats rock but loses to scissors
         // Scissors beats paper but loses to rock
     // Prompt asks user to input their choice Rock, Paper or Scissors
-    
-    /* let userChoice = prompt("Please pick Rock, Paper or Scissors.");  
-    let options = ["rock","paper","scissors"];
-    let choice = options.indexOf(userChoice.toLowerCase());
-    console.log(userChoice.toLowerCase());
-    console.log(options);
-    console.log(choice);
-    // Add in else, error please select Rock Paper or Scissors.
-    if (choice >= 0 && choice <= 2){
-        alert("Computing");
-    } else {
-        alert("Invalid response. Please pick Rock, Paper or Scissors.");
-    } */
-    
     // Computer randomly selects either Rock, Paper or Scissors
     // Computer compares the user's response to their own, aka calculates the winner
         // If user picks Rock &
@@ -45,11 +31,10 @@
             // Comp picks Paper, then user wins
             // Comp picks Scissors, then tie aka no winner
     // Output to user the result aka declare winner
-    // Ask user if they want to play again
-    // Repeat (RPS)
+    // Repeat game for 5 rounds or until someone wins a total of 3 games
 
-
-    let options = ["rock", "paper", "scissors"];
+// function game(){}
+    let options = ["rock", "paper", "scissors"]; 
     let computerChoice = getComputerChoice();
     function getComputerChoice(){
         const random = Math.floor(Math.random() * options.length);
@@ -61,12 +46,23 @@
         let userInput = prompt("Please pick Rock, Paper or Scissors");
         let choice = options.indexOf(userInput.toLowerCase());
         if(choice >= 0 && choice <= 2){
-            //compare(userChoice, computerChoice);
+            //playRound(userChoice, computerChoice);
         } else {
             alert("Invalid response.");
             getUserChoice();
         }
+        return userInput.toLowerCase();
     }
-    function compare(){ //pass arguments and name them
-        //switches
+    function playRound(userChoice, computerChoice){ 
+        if (userChoice === "rock" && computerChoice === "scissors"){
+            alert("Rock beats scissors, you win round 1!");
+            getUserChoice();
+        } else if (userChoice === "rock" && computerChoice === "paper"){
+            alert("Paper beats rock, you lose round 1.");
+            getUserChoice();
+        } else {
+            alert("Round 1 is a draw.")
+            getUserChoice();
+        }
     }
+    playRound(userChoice, computerChoice);
