ac3b503d8f097864e499d96c0f0fac3380811a60
Selena <selena.k.rodriguez@gmail.com>
2022-10-08 14:34:46 -0500

    Fixed if user input no response then a response by adding a return to our getUserChoice function. Shortened our computerChoice by removing the unnecessary function, left as comments.

--- script.js
+++ script.js
@@ -1,49 +1,15 @@
-// Rock Paper Scissors Instructions:
-    // Function getComputerChoice - this will randomly return either "Rock", "Paper", or "Scissors".
-        // Use the getComputerChoice function to have the computer play. (Check with console log before progressing)
-    // Need a function that plays a single round of Rock Paper Scissors. (playRound)
-        // Above function needs two parameters. (Make sure to return them to be used in later code.)
-            // playerSelection - needs to not be case sensitive (rock, Rock, RoCk, etc.) - I called this userChoice
-            // computerSelection - I called this computerChoice
-    // Return a string to declare the winner of the round. "You Lose! Paper beats Rock".
-    // Function game(), playRound function inside to play a 5 round game. It should keep score and report a winner or loser.
-        // use console.log() to display the results of each round and winner at the end.
-    // Use prompt() to get input from user.
-
-// My Rock Paper Scissors Game Steps
-    // RPS rules: 
-        // Rock beats scissors but loses to paper
-        // Paper beats rock but loses to scissors
-        // Scissors beats paper but loses to rock
-    // Prompt asks user to input their choice Rock, Paper or Scissors
-    // Computer randomly selects either Rock, Paper or Scissors
-    // Computer compares the user's response to their own, aka calculates the winner
-        // If user picks Rock &
-            // Comp picks Rock, then tie aka no winner
-            // Comp picks Paper, then user loses
-            // Comp picks Scissors, then user wins
-        // If user picks Paper &
-            // Comp picks Rock, then user wins
-            // Comp picks Paper, then tie aka no winner
-            // Comp picks Scissors, then user loses
-        // If user picks Scissors &
-            // Comp picks Rock, then user loses
-            // Comp picks Paper, then user wins
-            // Comp picks Scissors, then tie aka no winner
-    // Output to user the result aka declare winner
-    // Repeat game for 5 rounds or until someone wins a total of 3 games
-
 let compWins = 0;
 let userWins = 0;
 
 function game(){
     let options = ["rock", "paper", "scissors"]; 
-    let computerChoice = getComputerChoice();
-    function getComputerChoice(){
-        const random = Math.floor(Math.random() * options.length);
-        console.log("Computer picked: " + random, options[random]);
-        return options[random];
-    }
+    const random = Math.floor(Math.random() * options.length);
+    //let computerChoice = getComputerChoice();
+    let computerChoice = options[random];
+    
+    // function getComputerChoice(){
+    //     return options[random];
+    // }
     
     let userChoice = getUserChoice();
     function getUserChoice (){
@@ -51,11 +17,14 @@ function game(){
         let choice = options.indexOf(userInput.toLowerCase());
         if(choice < 0 || choice > 2){
             alert("Invalid response.");
-            getUserChoice();
+            return getUserChoice();
+        } 
+        else {
+            return userInput.toLowerCase();
         }
-        return userInput.toLowerCase();
     }
     console.log("User picked: " + userChoice);
+    console.log("Computer picked: " + options[random]);
     
     function playRound(userChoice, computerChoice){
         switch (userChoice){
@@ -96,11 +65,11 @@ function game(){
             case "scissors":
                 if (computerChoice === "paper"){
                     userWins++
-                    alert("Scissors beats paper, you win round 1!");
+                    alert("Scissors beats paper, you win the round!");
                     console.log("Scissors beats paper, user wins the round.");
                 } else if (computerChoice === "rock"){
                     compWins++
-                    alert("Rock beats scissors, you lose round 1.");
+                    alert("Rock beats scissors, you lose the round.");
                     console.log("Rock beats scissors, user loses the round.");
                 } else if (computerChoice === "scissors"){
                     alert("The round is a draw.");
@@ -122,4 +91,3 @@ while (userWins < 3 && compWins < 3){
     game();
     console.log("Round Ends: ");
 }
-// while computer wins does not equal 3 and while user wins does not equal 3, repeat the game
